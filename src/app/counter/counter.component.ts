7bd47960fcc205b684979452cb76c4c09545b540
AnderMichael <andercaymi@gmail.com>
2024-10-03 20:51:05 -0400

    task[03-10-2024]: afterRender Methods, early read, write, mixed, ReadWrite, read

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -9,10 +9,32 @@ import { Component, afterRender, afterNextRender } from '@angular/core';
 })
 export class CounterComponent {
   sum: number = 0;
-
+  counterBackground: string = 'Red';
   constructor() {
-    afterRender(() => {
-      console.log('AFTER RENDER: ', this.sum);
+    afterRender({
+      earlyRead: () => {
+        console.log("🚀 ~ CounterComponent ~ constructor ~ earlyRead:")
+        const currentAppColor = this.counterBackground;
+        return 'From early read: ' + currentAppColor;
+      },
+      write: (props) => {
+        console.log("🚀 ~ CounterComponent ~ constructor ~ write:")
+        document.body.style.backgroundColor = this.counterBackground;
+        return 'From write: ' + this.counterBackground;
+      },
+      mixedReadWrite: (props) => {
+        console.log("🚀 ~ CounterComponent ~ constructor ~ mixedReadWrite:")
+        if (props.includes('red')) {
+          this.counterBackground = 'green';
+        } else {
+          this.counterBackground = 'red';
+        }
+        return 'From mixedReadWrite: ' + this.counterBackground;
+      },
+      read: (props) => {
+        console.log("🚀 ~ CounterComponent ~ constructor ~ read:")
+        const newBackground = this.counterBackground;
+      },
     });
     afterNextRender(() => {
       console.log('AFTER NEXT RENDER: ', this.sum);
