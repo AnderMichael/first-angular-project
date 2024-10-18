20d2e3414b9f2e2daae11aaea0935c4de020a60c
AnderMichael <andercaymi@gmail.com>
2024-10-17 20:24:15 -0400

    task1[17-10-2024]: navigation from html

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ScoreComponent } from './score/score.component';
+import { ClassmateComponent } from './classmate/classmate.component';
+import { AverageComponent } from './average/average.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ScoreComponent,
+    children: [
+      {
+        path: 'average',
+        component: AverageComponent
+      }
+    ],
   },
   {
     path: 'classmate',
-    loadComponent: () => import('./classmate/classmate.component').then(n => n.ClassmateComponent)
-  }
+    component: ClassmateComponent,
+  },
 ];
 
 @NgModule({
