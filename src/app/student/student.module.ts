0a01ca6b0759d1f84fcab3aa0bd0ad195487df98
AnderMichael <andercaymi@gmail.com>
2024-10-24 21:07:09 -0400

    task2[24-10-2024]: simple service

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { ClassmateComponent } from './classmate/classmate.component';
 import { ScoreComponent } from './score/score.component';
 import { StudentRoutingModule } from './student-routing.module';
+import { ExamService } from './exam.service';
 
 
 @NgModule({
@@ -15,5 +16,8 @@ import { StudentRoutingModule } from './student-routing.module';
     CommonModule,
     StudentRoutingModule
   ],
+  providers:[
+    ExamService
+  ]
 })
 export class StudentModule { }
